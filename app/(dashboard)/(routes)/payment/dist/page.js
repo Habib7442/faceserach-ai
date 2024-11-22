0d3025d6e0bbd2cbc22f588ac064b39673f219ab
Habib7442 <habibtanwir1906@gmail.com>
2024-11-22 15:07:52 +0530

    added payment in landing page

--- app/(dashboard)/(routes)/payment/dist/page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var PricingSection_1 = require("@/components/PricingSection");
-var PaymentPage = function () {
-    return (React.createElement("div", null,
-        React.createElement(PricingSection_1["default"], null)));
-};
-exports["default"] = PaymentPage;
